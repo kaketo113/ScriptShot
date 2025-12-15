9febc23c58ccd712facfa3f3f5c6fc28141ea6bc
kaketo0113 <m.kaketo113@gmail.com>
2025-12-15 12:58:39 +0900

    import

---
+++ frontend/components/Sideber.tsx
@@ -0,0 +1,6 @@
+'use client'
+
+import React from 'react';
+import { Home, Search, PlusSquare, Bell, User, LogOut } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
