22404d0a8c3dd255631f9226e48d6ad36afeedee
kaketo0113 <m.kaketo113@gmail.com>
2025-12-17 12:24:35 +0900

    import

---
+++ frontend/app/post/[id]/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import React, { useState } from 'react';
+import { Sidebar } from '../../../components/Sidebar';
+import { 
+  ArrowLeft, Heart, MessageCircle, Share2, 
+  MoreHorizontal, Sparkles, Bot, User, Send 
+} from 'lucide-react';
+import Link from 'next/link';
+import { motion, AnimatePresence } from 'framer-motion';
