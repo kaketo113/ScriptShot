d68d58bb1d7e4a49a13ba5d87d04bd3928acea15
kaketo0113 <m.kaketo113@gmail.com>
2025-12-17 13:35:00 +0900

    import

---
+++ frontend/app/search/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import React, { useState } from 'react';
+import { Sidebar } from '../../components/Sidebar';
+import { Search, Hash, User, X } from 'lucide-react';
+import { motion, AnimatePresence } from 'framer-motion';
+import Link from 'next/link';
