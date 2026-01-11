e6c6430e0aa680ab0abb9f780bb7025a24e0a849
kaketo0113 <m.kaketo113@gmail.com>
2026-01-11 11:49:44 +0900

    基本カテゴリーのブロックを作成するための初期設定を追加

--- frontend/app/create/block/page.tsx
+++ frontend/app/create/block/page.tsx
@@ -3,3 +3,22 @@
 import React from 'react';
 import { BlocklyWorkspace } from 'react-blockly'; 
 import { Sidebar } from '@/components/Sidebar';
+
+export default function CreateBlockPage() {
+    const initalbox = {
+        kind: 'createBlock',
+        contents: [
+            {
+                kind:'category',
+                name: '基本',
+                colour: '#5C81A6',
+                contents: [
+                    {kind: 'block', type: 'controls_if'},//もし～なら
+                    {kind: 'block', type: 'logic_compare'},//比較
+                    {kind: 'block', type: 'math_number'},//数値
+                    {kind: 'block', type: 'text'},//テキスト
+                    {kind: 'block', type: 'text_print'},//表示
+                ],
+            },
+        ],
+    };
